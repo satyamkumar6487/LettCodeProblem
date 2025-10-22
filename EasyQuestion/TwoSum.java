a2486adb122064f65ed17057d302c0282d504159
satyamkumar6487 <jsatyamkumar597@gmail.com>
2025-10-22 00:42:28 -0700

    Initial commit

---
+++ EasyQuestion/TwoSum.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+    public static void main (String []args) {
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSums(nums, target);
+        System.out.println(Arrays.toString(result));
+
+
+    }
+
+    public  static int [] twoSums(int [] nums, int target){
+
+        HashMap<Integer , Integer> map =  new HashMap<>();
+
+        for (int i = 0; i<nums.length; i ++) {
+
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement) ,  i};
+            }
+
+
+            map.put(nums[i], i);
+
+
+        }
+        return new int[]{};
+    }
+}
