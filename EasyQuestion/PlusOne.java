a03933a9c991ab9bd2ea3c9839e3be4ca7276be1
satyamkumar6487 <jsatyamkumar597@gmail.com>
2025-11-03 11:41:19 -0800

    solve plusoneQuestion

---
+++ EasyQuestion/PlusOne.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class PlusOne {
+
+    public static void main(String [] args){
+
+int [] digits = {1,2,3};
+System.out.println(Arrays.toString(plusone(digits)));
+
+    }
+
+
+    static  int [] plusone (int [] digits){
+
+
+        int n = digits.length;
+        for (int i = n-1 ; i >= 0 ; i--){
+
+
+            if (digits[i] < 9){
+                digits[i] ++ ;
+                return  digits;
+            }
+
+            digits[i] = 0;
+
+        }
+
+        int [] newNumber = new int[n + 1];
+        newNumber[0] = 1 ;
+        return  newNumber ;
+    }
+}
