442beb30b0e37862d23dcdf9fc77aca74c462bfb
satyamkumar6487 <jsatyamkumar597@gmail.com>
2025-11-04 03:41:26 -0800

    solved singlenumber problem

---
+++ EasyQuestion/SingleNumber.java
@@ -0,0 +1,22 @@
+public class SingleNumber {
+
+    public static void main (String [] args){
+int []nums = {4,1,2,1,2};
+System.out.println(singleNumber(nums));
+
+    }
+
+
+    static  int singleNumber(int nums[]){
+
+        int value = 0;
+        for (int i = 0; i <nums.length; i ++) {
+
+            value = value^nums[i];
+        }
+        return  value;
+
+    }
+
+
+}
